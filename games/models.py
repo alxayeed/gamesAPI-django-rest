f88587da68a2e6301cb93d2cf7020eca1aa8a0e2
alxayeed <alxayeed@gmail.com>
2020-10-21 11:45:24 +0600

    model created for games

--- games/models.py
+++ games/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Games(models.Model):
+    name = models.CharField(max_length=200, blank=True, default="")
+    catagory = models.CharField(max_length=200, blank=True, default="")
+    played = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    release_date = models.DateTimeField(auto_now_add=True)
